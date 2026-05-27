3611589469a32030408eeeacb80b5d3deab656bc
fernando <fervelez977@gmail.com>
2026-05-27 09:02:13 -0500

    v0.0.1

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () =>
+            import('./pages/login/login.component')
+                .then(m => m.LoginComponent)
+    }
+
+];
